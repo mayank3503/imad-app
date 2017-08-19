1cb1c3b4cd7d3bc488bfe44ba402fa6f0eeb993d
mayank3503 <31033388+mayank3503@users.noreply.github.com>
2017-08-19 09:47:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,58 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne: {
+    title: 'Article One:Mayank Dobriyal';
+    heading:'Article One';
+    date:'18 August,2017';
+    content:` <p>
+                      this is Article One
+                  </p>
+                  <p>
+                      gulla chutiya hai gulla chutiya hai gulla chutiya hai gulla chutiya tha aur gulla chutiya rhega
+                  </p>
+                  <p>
+                      sab ke sab chutiye hai kaha bhadwo ke beech mai fas gya hun maisab ke sab chutiye hai kaha bhadwo ke beech mai fas gya hun maisab ke sab chutiye hai kaha bhadwo ke beech mai fas gya hun maisab ke sab chutiye hai kaha bhadwo ke beech mai fas gya hun maisab ke sab chutiye hai kaha bhadwo ke beech mai fas gya hun maisab ke sab chutiye hai kaha bhadwo ke beech mai fas gya hun maisab ke sab chutiye hai kaha bhadwo ke beech mai fas gya hun maisab ke sab chutiye hai kaha bhadwo ke beech mai fas gya hun mai'
+                   </p>
+            `;
+}
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var content=data.content;
+    var date=data.date;
+
+var htmlTemplate =`
+        <html>
+        <head>
+         <title>
+          ${title}
+         </title>
+        
+         <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+         <div class="container">
+          <div>
+           <a href="/">Home</a>
+           <hr/>
+          </div>
+          <h3>
+              ${heading}
+          </h3>
+          <div>
+              ${date}
+          </div>
+          <div>
+              ${content}
+            </div>
+        </div>
+        </body>
+        
+        </html>
+  `; 
+  return htmlTemplate;
+} 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -13,7 +65,7 @@ app.get('/article-one',function(req,res){
 });
 
 app.get('/article-two',function(req,res){
- res.send('Article one requested and will be served here');
+ res.send(createTemplate(articleOne0);
 });
 app.get('/article-three',function(req,res){
  res.send('Article one requested and will be served here');
