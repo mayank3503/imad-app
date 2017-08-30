ca1ed92c32e30583b15806071bb182287ef6d84f
mayank3503 <31033388+mayank3503@users.noreply.github.com>
2017-08-31 00:44:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var element= document.getElementById('main-text');
 element.innerHTML='hello u are awesome';
-var img = document.getElementById('img');
+var img = document.getElementById('madi');
 img.onclick = function(){
     img.style.marginLeft='100px';
 };
