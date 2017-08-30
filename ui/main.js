87a69ad07b186546424603ccb54c6dfe549b2649
mayank3503 <31033388+mayank3503@users.noreply.github.com>
2017-08-31 00:50:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,11 @@ console.log('Loaded!');
 var element= document.getElementById('main-text');
 element.innerHTML='hello u are awesome';
 var img = document.getElementById('madi');
+vr marginLeft=0;
+function moveRight{(){
+    marginLeft=maginLeft+10;
+    img.style.marginLeft= marginLeft +'px';
+}
 img.onclick = function(){
     var interval=setInteval(moveRight.100);
 };
