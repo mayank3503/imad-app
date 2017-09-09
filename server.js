6818c86b6e3aac2d4d578522d844c708ac82992c
mayank3503 <31033388+mayank3503@users.noreply.github.com>
2017-09-09 08:26:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,9 +122,22 @@ app.get('/counter',function(req,res){
      names.push(name);
      res.send(JSON.stringify(names));
  });
-app.get('/:articleName',function(req,res){
+app.get('/aticles/:articleName',function(req,res){
     var articleName = req.params.articleName;
- res.send(createTemplate(articles[articleName]));
+    pool.query("SELECT * FROM article WHERE title = " + req.params.articleName,function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        } else {
+            if(result.rows.length === 0){
+                res.status(404).send('Article not found');
+            }  else {
+                var articleData = result.rows(0);
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
+    
+  
 });
 
 
